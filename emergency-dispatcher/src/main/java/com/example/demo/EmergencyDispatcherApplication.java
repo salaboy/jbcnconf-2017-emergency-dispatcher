22c005c7ac15662a1d1bb3108c19c8f1f0fca1a3
Salaboy <salaboy@gmail.com>
2017-06-13 11:53:38 +0100

    updating dispatcher logic

--- emergency-dispatcher/src/main/java/com/example/demo/EmergencyDispatcherApplication.java
+++ emergency-dispatcher/src/main/java/com/example/demo/EmergencyDispatcherApplication.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Emergency;
 import com.example.demo.model.Patient;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,51 +43,85 @@ public class EmergencyDispatcherApplication {
         SpringApplication.run(EmergencyDispatcherApplication.class, args);
     }
 
-    @StreamListener(Sink.INPUT)
-    public void dispatchToProcedure(String emergency) {
-        logger.info("Received: " + emergency + " now need to dispatch based on type");
-        System.out.println("Received: " + emergency + " now need to dispatch based on type");
-        logger.info("Audit From Dispatcher: " + emergency.toString());
+    private Gson gson = new Gson();
 
-        System.out.println("Get Procedures: ");
+    /*
+     * In order to dispatch an Emergency to a specific procedure we need to:
+      *  1) calculate the "score" of the emergency based on the emergency properties
+     *  2) Look for all the available procedures
+     *  3) Match the procedure defined score with the calculated score
+     *  4) Pick the procedure that fits better based on the score
+     *  5) if there is no procedure that matches with the calculated score we should use the default procedure if it is available
+     */
+    @StreamListener(Sink.INPUT)
+    public void dispatchToProcedure(String emergencyJson) {
+        logger.info("Received: " + emergencyJson + " now need to dispatch based on type");
+        System.out.println("Received: " + emergencyJson + " now need to dispatch based on type");
+        logger.info("Audit From Dispatcher: " + emergencyJson.toString());
+
+        Emergency emergency = gson.fromJson(emergencyJson, Emergency.class);
+
+        Integer score  = calculateScore(emergency);
+
+        System.out.println("Emergency Score: " + score);
 
         List<String> services = discoveryClient.getServices();
+        System.out.println("Get Procedures: ");
         for(String s : services){
-            System.out.println("\t > Service: " + s);
             List<ServiceInstance> instances = discoveryClient.getInstances(s);
             URI selectedUri = null;
+            URI defaultUri = null;
             for(ServiceInstance instance : instances){
-                System.out.println("Filtering by producedures");
                 if(instance.getMetadata().containsKey("type") && instance.getMetadata().get("type").equals("procedure") ){
-
                     System.out.println("Service Instance Id: " + instance.getServiceId());
                     System.out.println("Service Instance MetaData: " + instance.getMetadata());
                     System.out.println("Service Instance Host: " + instance.getHost());
                     System.out.println("Service Instance Port: " + instance.getPort());
                     System.out.println("Service Instance URI: " + instance.getUri());
-                    if(instance.getServiceId().equalsIgnoreCase("procedure-a")){
-                        selectedUri = instance.getUri();
+                    if(instance.getServiceId().equalsIgnoreCase("procedure-default")){
+                        defaultUri = instance.getUri();
+                    }
+                    int procedureScoreValue = 0;
+                    if(instance.getMetadata().containsKey("score")) {
+                        String procedureScore = instance.getMetadata().get("score");
+                        if(procedureScore != null && !procedureScore.isEmpty()){
+                            procedureScoreValue = Integer.parseInt(procedureScore);
+                        }
+                        if(score == procedureScoreValue) {
+                            selectedUri = instance.getUri();
+                        }
                     }
                 }
+            }
+            if(selectedUri == null && defaultUri != null){
+                selectedUri = defaultUri;
             }
             if(selectedUri != null) {
                 System.out.println("Selected URI = " + selectedUri);
                 RestTemplate restTemplate = restTemplate();
                 String url = selectedUri + "/api/procedure";
-                //@TODO: Parse incoming string and create new emergency here
-                HttpEntity<Emergency> request = new HttpEntity<>(new Emergency(UUID.randomUUID().toString(),
-                        new Patient("salaboy")));
+                HttpEntity<Emergency> request = new HttpEntity<>(emergency);
                 ResponseEntity<Emergency> responseEntity = restTemplate.exchange(url,
                         HttpMethod.POST, request, Emergency.class);
-
 
                 if (responseEntity.getStatusCode() == HttpStatus.OK) {
                     System.out.println("Response from procedure: " + responseEntity);
                 } else {
                     System.out.println("Error calling procedure");
                 }
+            }else{
+                System.out.println("There is no procedure available at this time");
             }
+
         }
+    }
+
+    /*
+     * this calculate the score of the emergency based on the internal values
+     */
+    private Integer calculateScore(Emergency emergency) {
+
+        return 0;
     }
 
     private RestTemplate restTemplate() {
